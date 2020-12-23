eb60c4f652b28455674f392d5a731f5572067ebb
Tyler Lanigan <tylerlanigan@gmail.com>
2020-12-23 11:50:47 -0500

    converted to python one liner

--- top-interview-questions/array/contains-duplicate.py
+++ top-interview-questions/array/contains-duplicate.py
@@ -2,20 +2,13 @@ from typing import List
 
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-
-    	d = {}
-    	for x in nums:
-    		if x in d:
-    			return True
-    		d[x] = 1
-
-    	return False
+    	return len(set(nums)) == len(nums)
 
 
 
 
 sol = Solution()
 
-test = [1,2,3,4,1]
+test = [1,2,3,4,4]
 
 print(sol.containsDuplicate(test))
