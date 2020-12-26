f36025a9f8974c263bc7813c544800d34c7853ef
Tyler Lanigan <tylerlanigan@gmail.com>
2020-12-26 11:07:07 -0500

    did the recursive solution

--- top-interview-questions/linked-lists/merged.py
+++ top-interview-questions/linked-lists/merged.py
@@ -6,34 +6,18 @@
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
     
-        # A dummy node to store the result 
-        mergedHead = ListNode(0) 
-
-        # Tail stores the last node 
-        tail = mergedHead 
-        while True: 
-
-            # If any of the list gets completely empty 
-            # directly join all the elements of the other list 
-            if l1 is None: 
-                tail.next = l2 
-                break
-            if l2 is None: 
-                tail.next = l1 
-                break
-
-            # Compare the data of the lists and whichever is smaller is 
-            # appended to the last of the merged list and the head is changed 
-            if l1.val <= l2.val: 
-                tail.next = l1 
-                l1 = l1.next
-            else: 
-                tail.next = l2 
-                l2 = l2.next
-
-            # Advance the tail 
-            tail = tail.next
-
-        # Returns the head of the merged list 
-        return mergedHead.next
+        ## Recursive solution  
+        if l1 is None:
+            return l2
+        if l2 is None:
+            return l1
+        
+        if l1.val <= l2.val:
+            merged = l1
+            merged.next = self.mergeTwoLists(l1.next, l2)
+        else:
+            merged = l2
+            merged.next = self.mergeTwoLists(l1, l2.next)
+            
+        return merged
             
